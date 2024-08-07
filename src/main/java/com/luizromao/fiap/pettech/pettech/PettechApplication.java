0a2aae7a493819b8a06fa2119f4d90b8c0776f44
LuizRomao02 <luizromao7@outlook.com>
2024-08-07 19:01:08 -0300

    first commit

---
+++ src/main/java/com/luizromao/fiap/pettech/pettech/PettechApplication.java
@@ -0,0 +1,13 @@
+package com.luizromao.fiap.pettech.pettech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizromao/fiap/pettech/pettech/PettechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizromao.fiap.pettech.pettech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PettechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
